d124f1c87e53be16f091c3eb1623b7eeffb9fca2
Eden Mesfun <emesfun@gmail.com>
2018-01-24 14:01:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,9 +23,4 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  room_assignments = []
-  attendees.map.with_index do |attendees, index|
-    room_assignments << "Hello, #{attendees}! You'll be assigned to room #{index + 1}!"
-  end
-    room_assignments
 end
