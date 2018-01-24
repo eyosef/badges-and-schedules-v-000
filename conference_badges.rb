6c6f986a1bd6c67714ccb1e52d16126071c203f4
Eden Mesfun <emesfun@gmail.com>
2018-01-24 13:35:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -25,7 +25,7 @@ end #method
 def assign_rooms(attendees)
   room_assignments = []
   attendees.map! do |attendees|
-    room_assignments << "Hello, Edsger! You'll be assigned to room 1!"
+    room_assignments << "Hello, #{attendees}! You'll be assigned to room 1!"
 
 end
 
