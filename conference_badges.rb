73f61345ba2023566b1516c93c32e72ccb771d84
Eden Mesfun <emesfun@gmail.com>
2018-01-24 13:58:48 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,14 +6,6 @@ def badge_maker(name)
   end
 end
 
-# def batch_badge_creator(names)
-#     badges=[]
-#     names.each do |name|
-#       badges << "Hello, my name is #{name}."
-#     end #each
-#     badges
-# end #method #START HERE - RESEARCH map approach
-
 def batch_badge_creator(names)
     badges = []
     names.map! do |names|
