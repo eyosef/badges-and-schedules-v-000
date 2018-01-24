95ab3dd82f590ce1a3a6ec3a018d79daf165e409
Eden Mesfun <emesfun@gmail.com>
2018-01-24 13:12:30 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+require "pry"
+
+def badge_maker(name)
+  if name
+    return "Hello, my name is #{name}."
+  end
+end
+
+# def batch_badge_creator(names)
+#     badges=[]
+#     names.each do |name|
+#       badges << "Hello, my name is #{name}."
+#     end #each
+#     badges
+# end #method #START HERE - RESEARCH map approach
+
+def batch_badge_creator(names)
+    badges=[]
+    names.each do |name|
+      badges << "Hello, my name is #{name}."
+    end #each
+    badges
+end #method #START HERE - RESEARCH map approach
+
+def assign_rooms(attendees)
+end
+
+def printer
+end
